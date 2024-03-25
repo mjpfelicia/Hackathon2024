e15833ce5c5201ea297f33aeb5e777c789c3a23c
Sandra Felicio <mjpfelicia@gmail.com>
2024-03-25 19:22:15 -0300

    novo repositorio

---
+++ serve.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 3000;
+
+app.use(session({ secret: "1234567890" }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "./paginas"));
+app.use(express.static(__dirname + '/assets'));
+
+
+app.get("/", (req, res) => {
+
+  res.render("home");
+});
+
+app.get("/cadastrar", function (reg, res) {
+  res.render("cadastro.html");
+});
+
+app.get("/doar", function (reg, res) {
+  res.render("doar.html");
+});
+app.get("/junteSe", function (reg, res) {
+  res.render("junteSe.html");
+});
+
+app.get("/projeto", function (req, res) {
+  res.render("projetoSocial.html")
+})
+
+
+app.listen(port, () => {
+  console.log(`Servidor rodando http://localhost:${port}`);
+});
+
+
